d8150a044c6f9d75865d9ae4caf4acd449bc12f4
Kapish <mfkapish@gmail.com>
2026-07-22 12:00:05 +0530

    clean commit

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
-import os  # Unused import (Ruff should catch this)
+import os 
 import json
 
 def divide_by_zero():
-    return 1 / 0  # Logic bug (Groq should catch this)
+    return 1 / 0  
 
 def sql_injection():
     user_input = request.args.get("username")
     query = "SELECT * FROM users WHERE username = '" + user_input + "'"
-    cursor.execute(query)  # SQL Injection vulnerability
+    cursor.execute(query)  
 
 
 def test_func():
